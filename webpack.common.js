712451394abbf82f2955e6b37ba2618d1914f2c2
Tomasz Gajda <tomaszgajdait@gmail.com>
2020-11-03 10:31:59 +0100

    feat(prod and dev): added both prod and dev configs for webpack
    dev now with a working dev-server

--- webpack.common.js
+++ webpack.common.js
@@ -2,15 +2,8 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require("path");
 
 module.exports = {
-    mode: "development",
-    entry:"./src/index.js",
-    output: {
-        filename: "main.[contentHash].js",
-        path: path.resolve(__dirname, "dist")
-    },
-    plugins: [new HtmlWebpackPlugin({
-        template: "./src/template.html"
-    })],
+    entry: "./src/index.js",
+    plugins: [new HtmlWebpackPlugin({template: "./src/template.html"})],
     module: {
         rules: [
             {
